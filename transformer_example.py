330006f5fae7462ab5c19877982a6a71b04319c0
jloveric <john.loverich@gmail.com>
2021-01-24 08:25:55 -0800

    Moving to new machine.

--- transformer_example.py
+++ transformer_example.py
@@ -1,6 +1,8 @@
-import torch.nn as nn
+from torch import nn
+import torch
 
 transformer_model = nn.Transformer(nhead=16, num_encoder_layers=12)
 src = torch.rand((10, 32, 512))
 tgt = torch.rand((20, 32, 512))
 out = transformer_model(src, tgt)
+print(out)
